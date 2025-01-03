d8387fe2e9631c214afc0742572ca88fc99cbe91
TinsFox <fox@tinsfox.com>
2025-01-03 20:01:59 +0800

    refactor(login): simplify login page structure and integrate LoginForm component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,130 +1,18 @@
-"use client"
+import { GalleryVerticalEnd } from "lucide-react"
 
-import Image from "next/image"
-import Link from "next/link"
-import { useRouter, useSearchParams } from "next/navigation"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-import { useAuth } from "@/lib/auth-context"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
-const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  password: z.string().min(1, {
-    message: "Password must be at least 6 characters.",
-  }),
-})
+import { LoginForm } from "./components/login-form"
 
 export default function LoginPage() {
-  const { login } = useAuth()
-  const router = useRouter()
-  // const searchParams = useSearchParams()
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  })
-  const handleSubmit = async (data: z.infer<typeof FormSchema>) => {
-    try {
-      await login(data.username, data.password)
-      // const redirect = searchParams.get("redirect")
-      router.push("/dashboard")
-    } catch (error) {
-      console.error("Login failed:", error)
-    }
-  }
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-        <div className="flex items-center justify-center py-12">
-          <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Nextale</h1>
-              <p className="text-balance text-muted-foreground">
-                Login to your admin
-              </p>
-            </div>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(handleSubmit)}
-                className="grid gap-4"
-              >
-                <div className="grid gap-2">
-                  <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Username" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="Password"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </form>
-            </Form>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link href="#" className="underline">
-                Sign up
-              </Link>
-            </div>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <h1 className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <GalleryVerticalEnd className="size-4" />
           </div>
-        </div>
-        <div className="hidden bg-muted lg:block">
-          <Image
-            src="/placeholder.svg"
-            alt="Image"
-            width="1920"
-            height="1080"
-            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </div>
+          Nextale Dashboard
+        </h1>
+        <LoginForm />
       </div>
     </div>
   )
